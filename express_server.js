3f7406fd25db3f2e0b6636a08866447da1de4aba
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-08 23:17:36 +0000

    added a second page and header.  Completed 'template engine' assignment

--- express_server.js
+++ express_server.js
@@ -36,6 +36,12 @@ app.get("/set", (req, res) => {
   res.render("urls_index", templateVars);
  });
 
+ app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; // longURL same as short?
+  console.log('test', templateVars);
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
